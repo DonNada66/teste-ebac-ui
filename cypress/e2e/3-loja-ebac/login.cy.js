7dba2a9df4538dcff6d7f05f66139d4a5b771821
Gabriel Donada <gabrieldonada471@gmail.com>
2025-10-20 17:54:49 -0300

    Inserido ocasião de erro

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -10,4 +10,16 @@ describe('Funcionalidade: Login', () => {
 
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, gabriel-1629 (não é gabriel-1629? Sair)')
     })
+
+    it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('grabriel@ebac.com')
+        cy.get('#password').type('teste123@')
+        cy.get('.woocommerce-form > .button').click()
+
+        //cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido.')
+        cy.get('.woocommerce-error').should('exist')
+    })
 })
+
+        
